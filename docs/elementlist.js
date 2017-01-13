7d0ade18b30f0b98176931591ee0b89ae4d70c05
rez1dent3 <maksim.babichev95@gmail.com>
2017-01-13 14:24:32 +0300

    add docs

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Deimos\\Cookie\\Cookie"],["c","Deimos\\Cookie\\Extension"],["c","Deimos\\Cookie\\Extensions\\Flash"],["c","Deimos\\Cookie\\Extensions\\Option"],["c","Deimos\\Cookie\\Extensions\\Variable"],["c","Deimos\\Cookie\\SecureDefault"],["c","Exception"],["c","InvalidArgumentException"],["c","LogicException"],["c","Throwable"]];
